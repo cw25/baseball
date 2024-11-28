20e24b8eeacb7d95fdc78c465639c468c4c99e79
Chris Walker <iamchristopherwalker@gmail.com>
2024-11-27 16:47:34 -0800

    Fix team name mapping

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,7 +1,37 @@
 export function teamName(teamCode) {
   const teamNameMap = {
+    "ALS": "AL All-Stars",
+    "ANA": "Angels",
+    "ARI": "Diamondbacks",
+    "ATL": "Braves",
+    "BAL": "Orioles",
     "BOS": "Red Sox",
-    "LAN": "Dodgers", // #02319b / #ffffff
+    "CHA": "White Sox",
+    "CHN": "Cubs",
+    "CIN": "Reds",
+    "CLE": "Guardians",
+    "COL": "Rockies",
+    "DET": "Tigers",
+    "HOU": "Astros",
+    "KCA": "Royals",
+    "LAN": "Dodgers",
+    "MIA": "Marlins",
+    "MIL": "Brewers",
+    "MIN": "Twins",
+    "NLS": "NL All-Stars",
+    "NYA": "Yankees",
+    "NYN": "Mets",
+    "OAK": "A's",
+    "PHI": "Phillies",
+    "PIT": "Pirates",
+    "SDN": "Padres",
+    "SEA": "Mariners",
+    "SFN": "Giants",
+    "SLN": "Cardinals",
+    "TBA": "Rays",
+    "TEX": "Rangers",
+    "TOR": "Blue Jays",
+    "WAS": "Nationals",
   };
 
   if (!teamNameMap[teamCode]) {
@@ -14,3 +44,4 @@ export function teamName(teamCode) {
 export function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
